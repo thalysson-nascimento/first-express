2070966c206c664cab32817e85f93dd617234ae5
thalysson1987 <thalyssonwg.nascimento@gmail.com>
2019-06-27 21:29:50 -0300

    chore: adcionando a rota /users

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const server = express();
 
+const users = ['Thalysson Nascimento', 'Julio Morais', 'Carlos Barbosa'];
+
 server.get('/', (req, res) => {
     return res.json({ message: "API 1.0" });
+})
+
+server.get('/user', (req, res) => {
+    return res.json({ users });
 })
 
 server.listen("Rodando na porta 5000", 5000)
